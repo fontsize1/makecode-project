3971a66f85e5cd36ebbbbb79f91734f5f5e5a551
McBiggie <93778239+McBiggie@users.noreply.github.com>
2021-12-13 18:05:08 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-game.splash("My name is Giovanni", "Giorgio, but...")
-game.splash("Everybody just calls me...")
-game.splash("Giorgio")
+tiles.setTilemap(tilemap`level2`)
+forever(function () {
+    scene.centerCameraAt(scene.cameraProperty(CameraProperty.X) + 2, scene.cameraProperty(CameraProperty.Y) + 2)
+})
+forever(function () {
+    if (140 < scene.cameraProperty(CameraProperty.X)) {
+        scene.centerCameraAt(0, 0)
+    }
+})
