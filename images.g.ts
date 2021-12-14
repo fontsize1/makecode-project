1c4e2f021bf6619c58475c2fd992b7bf3a8e0ddb
McBiggie <93778239+McBiggie@users.noreply.github.com>
2021-12-13 19:41:42 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,75 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Cursor":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . c . . . . . . . . . 
+. . . . . c 1 c . . . . . . . . 
+. . . . . c 1 c . . . . . . . . 
+. . . . . c 1 c . . . . . . . . 
+. . . . . c 1 c . . . . . . . . 
+. . . . . c 1 c c c c c . . . . 
+. . . c c c 1 1 c 1 c 1 c . . . 
+. . . c 1 c 1 b 1 b 1 b c . . . 
+. . . c b 1 1 b 1 b 1 b c . . . 
+. . . c b b 1 1 1 1 b b c . . . 
+. . . . c c b 1 1 1 c c c . . . 
+. . . . c b b b b 1 b b c . . . 
+. . . . . c c c c c c c . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "6qI+#?8v`aMfv-^}lk]f":
+            case "Start Button":return img`
+44444444444444444444444444444444
+4dddddddddddddddddddddddddddddd4
+4d4444444444444444444444444444d4
+4d4444444444444444444444444444d4
+4d4444111141444441411141114444d4
+4d4444144444144414441444144444d4
+4d4444144444144414441444144444d4
+4d4444144444414144441444144444d4
+4d4444111144414144441444144444d4
+4d4444144444441444441444144444d4
+4d4444144444414144441444144444d4
+4d4444144444414144441444144444d4
+4d4444111144144414411144144444d4
+4d4444444444444444444444444444d4
+4dddddddddddddddddddddddddddddd4
+44444444444444444444444444444444
+`;
+            case "image2":
+            case "Start Button0":return img`
+44444444444444444444444444444444
+4dddddddddddddddddddddddddddddd4
+4d4444444444444444444444444444d4
+4d4444444444444444444444444444d4
+4d4444444444444444444444444444d4
+4d4441114444444444444444444444d4
+4d4414441411144414441114411144d4
+4d4414444441444141441441441444d4
+4d4441114441444141441411441444d4
+4d4444441441444111441144441444d4
+4d4414441441441444141414441444d4
+4d4441114441441444141441441444d4
+4d4444444444444444444444444444d4
+4d4444444444444444444444444444d4
+4dddddddddddddddddddddddddddddd4
+44444444444444444444444444444444
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,12 +1,75 @@
 namespace SpriteKind {
     export const Button = SpriteKind.create()
 }
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Location == 0) {
+        if (Button == 0) {
+            Button = 2
+            music.baDing.play()
+        }
+        if (Button == 1) {
+            Button = 2
+            music.baDing.play()
+        }
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Location == 0) {
+        if (Button == 2) {
+            Location = 1
+            music.powerUp.play()
+        }
+    }
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Location == 0) {
+        if (Button == 2) {
+            Button = 1
+        }
+    }
+})
+let Button = 0
+let Location = 0
+Location = 0
 tiles.setTilemap(tilemap`level2`)
+let StartButton = sprites.create(assets.image`Start Button0`, SpriteKind.Button)
+let ExitButton = sprites.create(assets.image`Start Button`, SpriteKind.Button)
+let Cursor = sprites.create(assets.image`Cursor`, SpriteKind.Player)
+StartButton.setStayInScreen(true)
+ExitButton.setStayInScreen(true)
+ExitButton.setPosition(80, 80)
 forever(function () {
-    scene.centerCameraAt(scene.cameraProperty(CameraProperty.X) + 2, scene.cameraProperty(CameraProperty.Y) + 2)
+    if (Location == 0) {
+        scene.centerCameraAt(scene.cameraProperty(CameraProperty.X) + 1, scene.cameraProperty(CameraProperty.Y) + 1)
+        StartButton.x += 1
+        StartButton.y += 1
+        ExitButton.x += 1
+        ExitButton.y += 1
+        if (140 < scene.cameraProperty(CameraProperty.X)) {
+            scene.centerCameraAt(0, 0)
+            StartButton.setPosition(80, 60)
+            ExitButton.setPosition(80, 80)
+        }
+    } else {
+        ExitButton.destroy()
+        StartButton.destroy()
+        Cursor.destroy()
+    }
 })
 forever(function () {
-    if (140 < scene.cameraProperty(CameraProperty.X)) {
-        scene.centerCameraAt(0, 0)
+    if (Button == 2) {
+        Cursor.destroy()
+        Cursor = sprites.create(assets.image`Cursor`, SpriteKind.Player)
+        Cursor.setStayInScreen(true)
+        Cursor.setPosition(StartButton.x, StartButton.y + 8)
+    }
+    if (Button == 0) {
+        Cursor.destroy()
+    }
+    if (Button == 1) {
+        Cursor.destroy()
+        Cursor = sprites.create(assets.image`Cursor`, SpriteKind.Player)
+        Cursor.setStayInScreen(true)
+        Cursor.setPosition(ExitButton.x, ExitButton.y + 8)
     }
 })
